fba954d314513f65cf166b2846ca2764a738be7d
vinodmadhu97 <vinodmadhu97@gmail.com>
2022-03-23 15:33:43 +0530

    Test : Notification with Big picture

--- lib/test.dart
+++ lib/test.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:donor_app/screens/main/BMI_calculation_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 import 'package:rxdart/subjects.dart';
 
 class NotificationTest extends StatefulWidget {
@@ -84,10 +88,35 @@ class NotificationApi {
   }
 
   static Future _notificationDetails() async {
+    var uri = Uri.https("www.picsum.photos", "/200/300");
+    var bigPicturePath = await Utils.downloadFile(uri, "bigPicture");
+    /*var largeIconPath = await Utils.downloadFile("", "largeIcon");*/
+
+    var styleInformation = BigPictureStyleInformation(
+      FilePathAndroidBitmap(
+          bigPicturePath), /*largeIcon: FilePathAndroidBitmap(largeIconPath)*/
+    );
     return NotificationDetails(
         android: AndroidNotificationDetails("channel id", "channel name",
             channelDescription: "channel description",
-            importance: Importance.max),
+            importance: Importance.max,
+            styleInformation: styleInformation),
         iOS: IOSNotificationDetails());
+  }
+}
+
+class Utils {
+  static Future<String> downloadFile(Uri url, String filename) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/$filename';
+
+    final response = await http.get(url);
+    print(response.statusCode);
+    print(response.body);
+    final file = File(filePath);
+
+    await file.writeAsBytes(response.bodyBytes);
+
+    return filePath;
   }
 }
