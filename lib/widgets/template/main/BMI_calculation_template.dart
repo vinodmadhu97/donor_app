3bc484e5a95e18d0e211c913e1caab16f6f0e16c
Devindi Jayawardena <devindijayawardena.dj@gmail.com>
2022-06-23 16:22:56 +0530

    Updated: BMI issue

--- lib/widgets/template/main/BMI_calculation_template.dart
+++ lib/widgets/template/main/BMI_calculation_template.dart
@@ -32,196 +32,198 @@ class _BMICalculationTemplateState extends State<BMICalculationTemplate> {
         title: Text("BMI " + "CALCULATOR".tr),
         centerTitle: true,
       ),
-      body: SafeArea(
-        child: Column(children: [
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              BMICardView(
-                //cardSelectedColor: maleCardColor,
-                cardSelectedColor: selectedGender == Gender.male
-                    ? Constants.appColorBrownRed
-                    : Constants.appColorWhite,
-                cardChild: IconContent(
-                    genderIcon: FontAwesomeIcons.male, genderName: "MALE".tr),
-                myOnpressed: () {
-                  setState(() {
-                    selectedGender = Gender.male;
-                    gender = "male";
-                  });
-                },
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                BMICardView(
+                  //cardSelectedColor: maleCardColor,
+                  cardSelectedColor: selectedGender == Gender.male
+                      ? Constants.appColorBrownRed
+                      : Constants.appColorWhite,
+                  cardChild: IconContent(
+                      genderIcon: FontAwesomeIcons.male, genderName: "MALE".tr),
+                  myOnpressed: () {
+                    setState(() {
+                      selectedGender = Gender.male;
+                      gender = "male";
+                    });
+                  },
+                ),
+                BMICardView(
+                  cardSelectedColor: selectedGender == Gender.female
+                      ? Constants.appColorBrownRed
+                      : Constants.appColorWhite,
+                  cardChild: IconContent(
+                      genderIcon: FontAwesomeIcons.female,
+                      genderName: "FEMALE".tr),
+                  myOnpressed: () {
+                    setState(() {
+                      selectedGender = Gender.female;
+                      gender = "female";
+                    });
+                  },
+                ),
+              ],
+            ),
+            BMICardView(
+              cardSelectedColor: Constants.appColorWhite,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("HEIGHT".tr),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text("${height.toInt()}",
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w900,
+                          )),
+                      Text("cm")
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0),
+                        activeTickMarkColor: Constants.appColorBrownRed,
+                        activeTrackColor: Constants.appColorBrownRed),
+                    child: Slider(
+                        value: height.toDouble(),
+                        min: 0.0,
+                        max: 250.0,
+                        onChanged: (value) {
+                          setState(() {
+                            height = value.toInt();
+                          });
+                        }),
+                  ),
+                ],
               ),
-              BMICardView(
-                cardSelectedColor: selectedGender == Gender.female
-                    ? Constants.appColorBrownRed
-                    : Constants.appColorWhite,
-                cardChild: IconContent(
-                    genderIcon: FontAwesomeIcons.female,
-                    genderName: "FEMALE".tr),
-                myOnpressed: () {
-                  setState(() {
-                    selectedGender = Gender.female;
-                    gender = "female";
-                  });
-                },
+            ),
+            BMICardView(
+              cardSelectedColor: Constants.appColorWhite,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("WEIGHT".tr),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text("${weight}",
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w900,
+                          )),
+                      Text("kg")
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0),
+                        activeTickMarkColor: Constants.appColorBrownRed,
+                        activeTrackColor: Constants.appColorBrownRed),
+                    child: Slider(
+                        value: weight.toDouble(),
+                        min: 0.0,
+                        max: 300.0,
+                        onChanged: (value) {
+                          setState(() {
+                            weight = value.toInt();
+                          });
+                        }),
+                  ),
+                ],
               ),
-            ],
-          ),
-          BMICardView(
-            cardSelectedColor: Constants.appColorWhite,
-            cardChild: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("HEIGHT".tr),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
-                    Text("${height.toInt()}",
-                        style: TextStyle(
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w900,
-                        )),
-                    Text("cm")
-                  ],
-                ),
-                SliderTheme(
-                  data: SliderTheme.of(context).copyWith(
-                      thumbShape:
-                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                      overlayShape:
-                          RoundSliderOverlayShape(overlayRadius: 30.0),
-                      activeTickMarkColor: Constants.appColorBrownRed,
-                      activeTrackColor: Constants.appColorBrownRed),
-                  child: Slider(
-                      value: height.toDouble(),
-                      min: 0.0,
-                      max: 250.0,
-                      onChanged: (value) {
-                        setState(() {
-                          height = value.toInt();
-                        });
-                      }),
-                ),
-              ],
             ),
-          ),
-          BMICardView(
-            cardSelectedColor: Constants.appColorWhite,
-            cardChild: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("WEIGHT".tr),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
-                    Text("${weight}",
-                        style: TextStyle(
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w900,
-                        )),
-                    Text("kg")
-                  ],
-                ),
-                SliderTheme(
-                  data: SliderTheme.of(context).copyWith(
-                      thumbShape:
-                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                      overlayShape:
-                          RoundSliderOverlayShape(overlayRadius: 30.0),
-                      activeTickMarkColor: Constants.appColorBrownRed,
-                      activeTrackColor: Constants.appColorBrownRed),
-                  child: Slider(
-                      value: weight.toDouble(),
-                      min: 0.0,
-                      max: 300.0,
-                      onChanged: (value) {
-                        setState(() {
-                          weight = value.toInt();
-                        });
-                      }),
-                ),
-              ],
+            BMICardView(
+              cardSelectedColor: Constants.appColorWhite,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Age".tr),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text("${age}",
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w900,
+                          )),
+                      Text("Years".tr)
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0),
+                        activeTickMarkColor: Constants.appColorBrownRed,
+                        activeTrackColor: Constants.appColorBrownRed),
+                    child: Slider(
+                        value: age.toDouble(),
+                        min: 0.0,
+                        max: 120.0,
+                        onChanged: (value) {
+                          setState(() {
+                            age = value.toInt();
+                          });
+                        }),
+                  ),
+                ],
+              ),
             ),
-          ),
-          BMICardView(
-            cardSelectedColor: Constants.appColorWhite,
-            cardChild: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Age".tr),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: [
-                    Text("${age}",
-                        style: TextStyle(
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w900,
-                        )),
-                    Text("Years".tr)
-                  ],
-                ),
-                SliderTheme(
-                  data: SliderTheme.of(context).copyWith(
-                      thumbShape:
-                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                      overlayShape:
-                          RoundSliderOverlayShape(overlayRadius: 30.0),
-                      activeTickMarkColor: Constants.appColorBrownRed,
-                      activeTrackColor: Constants.appColorBrownRed),
-                  child: Slider(
-                      value: age.toDouble(),
-                      min: 0.0,
-                      max: 120.0,
-                      onChanged: (value) {
-                        setState(() {
-                          age = value.toInt();
-                        });
-                      }),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30),
-            child: BottomButton(
-              btnTitleText: "CALCULATE".tr,
-              onTapOp: () {
-                print(height);
-                print(weight);
-                print(age);
-                print(gender);
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30),
+              child: BottomButton(
+                btnTitleText: "CALCULATE".tr,
+                onTapOp: () {
+                  print(height);
+                  print(weight);
+                  print(age);
+                  print(gender);
 
-                if (gender == null) {
-                  Constants().showToast("Please Select a gender".tr);
-                } else if (!(height > 0)) {
-                  Constants().showToast("Please Select a height".tr);
-                } else if (!(weight > 0)) {
-                  Constants().showToast("Please Select a weight".tr);
-                } else if (!(age > 0)) {
-                  Constants().showToast("Please Select a age".tr);
-                } else {
-                  //CalculateBMI calculateBMI = CalculateBMI(height: height,weight: weight,gender: gender!);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BMIResultScreen(
-                          height: height, weight: weight, gender: gender),
-                    ),
-                  );
-                }
-              },
+                  if (gender == null) {
+                    Constants().showToast("Please Select a gender".tr);
+                  } else if (!(height > 0)) {
+                    Constants().showToast("Please Select a height".tr);
+                  } else if (!(weight > 0)) {
+                    Constants().showToast("Please Select a weight".tr);
+                  } else if (!(age > 0)) {
+                    Constants().showToast("Please Select a age".tr);
+                  } else {
+                    //CalculateBMI calculateBMI = CalculateBMI(height: height,weight: weight,gender: gender!);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => BMIResultScreen(
+                            height: height, weight: weight, gender: gender),
+                      ),
+                    );
+                  }
+                },
+              ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     );
   }
